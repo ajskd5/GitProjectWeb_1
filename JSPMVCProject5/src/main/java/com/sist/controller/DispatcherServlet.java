1472fac8293fd469fb650b823ed4ffe12736af4f
ajskd5 <ajskd5@naver.com>
2022-07-28 09:38:58 +0900

    1

--- JSPMVCProject5/src/main/java/com/sist/controller/DispatcherServlet.java
+++ JSPMVCProject5/src/main/java/com/sist/controller/DispatcherServlet.java
@@ -17,7 +17,7 @@ import com.sist.model.*;
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	List<String> clsList = new ArrayList<String>();
-	
+
 	public void init(ServletConfig config) throws ServletException {
 		clsList.add("com.sist.model.FoodModel");
 	}
