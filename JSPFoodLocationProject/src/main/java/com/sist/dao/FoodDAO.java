1529d7b61acfda79185d16e50211e95fe0400ac0
ajskd5 <ajskd5@naver.com>
2022-08-12 14:22:41 +0900

    08/12 지도이미지로 검색

---
+++ JSPFoodLocationProject/src/main/java/com/sist/dao/FoodDAO.java
@@ -0,0 +1,34 @@
+package com.sist.dao;
+
+import java.io.Reader;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import java.util.*;
+
+public class FoodDAO {
+	private static SqlSessionFactory ssf;
+	static {
+		try {
+			Reader reader = Resources.getResourceAsReader("Config.xml");
+			ssf = new SqlSessionFactoryBuilder().build(reader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * <select id="foodFindData" resultType="com.sist.dao.FoodVO" parameterType="String">
+		SELECT * FROM food_location
+		WHERE address LIKE '%'||#{address}||'%'
+	   </select>
+	 */
+	public static List<FoodVO> foodFindData(String address){
+		SqlSession session = ssf.openSession();
+		List<FoodVO> list = session.selectList("foodFindData", address);
+		session.close();
+		return list;
+	}
+	
+}
